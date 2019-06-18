0d74f19eae5518176a08748dbfdda80cbcb64d5f
sonny-llc <me@sonny.llc>
2019-06-17 19:18:38 -0500

    mighta fixed it

--- src/app.js
+++ src/app.js
@@ -27,15 +27,15 @@ app.get('', (req, res) => {
     });
 });
 
-app.get('/help', (req, res) => {
+app.get('/about', (req, res) => {
     res.render('about', {
         title: 'About Me',
         name: 'Sonny Gale',
     });
 });
 
-app.get('/about', (req, res) => {
-    res.render('Help', {
+app.get('/help', (req, res) => {
+    res.render('help', {
         helpText: 'This is some helpful text.',
         title: 'Help',
         name: 'Sonny Gale',
